d76800a2b35efcd17ee1728bbc9432f40d4a8440
Jareth Harmon <jarethharmon@gmail.com>
2022-05-29 23:41:22 -0500

    add option to choose preview image with left click

--- godot/my-projects/image-tagger/scripts/preview_image.gd
+++ godot/my-projects/image-tagger/scripts/preview_image.gd
@@ -1,13 +1,30 @@
 extends VBoxContainer
 
 const BASE_MAX_DIMENSIONS=16384
-const icon = preload("res://misc/icon.png")
 
 # export (NodePath) var ListItems ; onready var item_list:ItemList = get_node(ListItems)
 export (NodePath) var ViewportDisplay ; onready var viewport_display = get_node(ViewportDisplay)
+export (NodePath) var FileD ; onready var fd:FileDialog = get_node(FileD)
 
-func _ready() -> void:
-	$hbox_0/image_0.texture = icon
+func _input(event) -> void:
+	if fd.visible: return
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT: 
+			fd.mode = 0
+			fd.access = 2
+			fd.window_title = "Choose an image"
+			fd.popup()
+			
+func _on_FileDialog_file_selected(path) -> void:
+	var i:Image = Image.new()
+	var e:int = i.load(path)
+	if e != OK: return
+	
+	var it:ImageTexture = ImageTexture.new()
+	it.create_from_image(i, 0)
+	it.set_size_override(calc_size(it))
+	$hbox_0/image_0.texture = it
+	fd.hide()
 
 func calc_size(it:ImageTexture) -> Vector2:
 	var size_1:Vector2 = viewport_display.rect_size
@@ -31,3 +48,6 @@ func calc_size(it:ImageTexture) -> Vector2:
 		if ratio_s.y < ratio_s.x: size *= ratio_s.y
 		else: size *= ratio_s.x
 	return size
+
+
+
