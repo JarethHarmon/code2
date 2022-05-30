a5f59b2f8b29b707e71426364adae56cc505ccc8
Jareth Harmon <jarethharmon@gmail.com>
2022-05-29 20:13:45 -0500

    add solution, and a dll
    - AnimatedImages.dll is an MIT-licensed project that I edited to properly coalesce APNG files.

---
+++ godot/my-projects/image-tagger/global/ImageOp.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using Alphaleonis.Win32.Filesystem;
+using ImageMagick;
+using CoenM.ImageHash;
+using CoenM.ImageHash.HashAlgorithms;
+
+public class ImageOp : Node
+{
+	public void CalcDifferenceHash(string path) {
+		var stream = SixLabors.ImageSharp.Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(path);
+		var algo = new DifferenceHash();
+		ulong hash = algo.Hash(stream);
+		GD.Print(hash);
+	}
+}
